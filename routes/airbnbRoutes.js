9766e9d6916f0c3fdd82d9c77af4b0e71802a7bc
Nisar Khan <khannisarnk0786@gmail.com>
2024-07-02 18:50:08 +0530

    developed the dynamic search API

--- routes/airbnbRoutes.js
+++ routes/airbnbRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { getProperty_type, searchByname } from '../controllers/AirBnbController.js'
+import { getFilteredData, getProperty_type, searchByname } from '../controllers/AirBnbController.js'
 
 const router = express.Router()
 
 router.get('/propertyType_list', getProperty_type)
 router.get('/searchType', searchByname)
+router.get('/getFilteredData', getFilteredData)
 
 
 export default router
